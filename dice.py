c73e1f56d88054ac231112fd940f12b6b100d51c
YourFriendlyUniverse <145475832+YourFriendlyUniverse@users.noreply.github.com>
2024-05-10 11:02:51 -0400

    Add files via upload
    added scaling functionality (broken)

--- dice.py
+++ dice.py
@@ -3,28 +3,47 @@ import random
 
 
 class Dice:
-    def __init__(self, sides):
+    def __init__(self, sides, times_scaled):
+        # basic functionality
         self.faces = []
         for i in range(sides):
             self.faces.append(i + 1)
         self.sides = sides
         self.side_up = random.randint(1, self.sides)
-        self.face_up_number = self.faces[self.side_up - 1]  # gets the number that's face up
-        self.image = pygame.image.load(f"{self.face_up_number}Dice.png")    # gets the image
+        self.face_up_symbol = self.faces[self.side_up - 1]  # gets the number that's face up
+        self.image = pygame.image.load(f"{self.face_up_symbol}Dice.png")    # gets the image
         self.image_size = self.image.get_size()
+        # scaling
+        self.scale_factor = 2
+        self.original_image = self.image    # sets the original image to be used for scaling
+        self.image_size = self.image_size * (2 ** times_scaled)
+        self.image = pygame.transform.scale(self.original_image, (self.image_size[0] / (self.scale_factor ** times_scaled), self.image_size[1] / (self.scale_factor ** times_scaled)))
+        self.original_image_size = self.image.get_size()  # sets the original image to be used for scaling
+        # rectangles for display
         self.rect = pygame.Rect(100, 100, self.image_size[0], self.image_size[1])   # sets location of dice
         self.rect.center = (100, 100)   # sets center of dice
 
     def roll_dice(self):
         self.side_up = random.randint(1, self.sides)
-        self.face_up_number = self.faces[self.side_up - 1]
-        self.image = pygame.image.load(f"{self.face_up_number}Dice.png")
+        self.face_up_symbol = self.faces[self.side_up - 1]
+        self.image = pygame.image.load(f"{self.face_up_symbol}Dice.png")
         # updates image and rolls dice
 
     def position_correct(self, previous_x, previous_y, start, limit):
         # previous_x and previous_y are the x and y positions of the center of the previous dice
-        if not previous_x >= limit[0] - 100:
-            self.rect.center = (previous_x + self.image_size[0], previous_y)
-        elif previous_y <= limit[1]:
-            # loops back around to beginning but one row down
-            self.rect.center = (start[0], previous_y + self.image_size[1])
+        if previous_x < limit[0] and previous_y < limit[1]:
+            if not previous_x >= limit[0] - 100:
+                # places dice to the right of the previous one
+                self.rect.center = (previous_x + self.image_size[0], previous_y)
+            elif previous_y <= limit[1]:
+                # loops back around to beginning but one row down
+                self.rect.center = (start[0], previous_y + self.image_size[1])
+        else:
+            return True
+            # returns true to let program know to scale dice
+
+    def scale_down(self):
+        self.image = pygame.transform.scale(self.original_image, (self.image_size[0] / self.scale_factor, self.image_size[1] / self.scale_factor))
+        self.rect = self.image.get_rect()
+        self.image_size = self.image.get_size()
+
--- main.py
+++ main.py
@@ -13,8 +13,17 @@ def die_position_correct(die_list, screen_size):
     for i in range(len(die_list)):
         if i == 0:
             die_list[0].rect_center = (100, 100)
-        else:
-            die_list[i].position_correct(die_list[i - 1].rect.center[0], die_list[i - 1].rect.center[1], (100, 100), screen_size)
+        elif die_list[i].position_correct(die_list[i - 1].rect.center[0], die_list[i - 1].rect.center[1], (100, 100), screen_size):
+            for dice in die_list:
+                dice.scale_down()
+            return "rerun"
+
+
+def add_dice_total(die_list):
+    total = 0
+    for dice in die_list:
+        total += dice.face_up_symbol
+    return total
 
 
 pygame.init()
@@ -28,7 +37,7 @@ settings = {
 }
 
 # initializing variables
-my_font = pygame.font.SysFont(settings["font"], 20)
+display_font = pygame.font.SysFont(settings["font"], 20)
 pygame.display.set_caption("Dice roller")
 title_screen = True
 size = settings["screen_size"]
@@ -48,6 +57,9 @@ add_dice_button = MenuButton("AddDice", (screen_center[0], screen_center[1] + (s
 
 # dice game variables
 all_dice = []
+dice_total = 0
+display_dice_total = display_font.render(f"{dice_total}", True, (255, 255, 255))
+times_scaled = 0
 
 while run:
     # --- Main event loop
@@ -64,19 +76,24 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if dice_option.rect.collidepoint(event.pos) and not dice_option.clicked:    # checks for a click on the dice gamemode option
                 dice_option.clicked = True
-                all_dice = [Dice(6)]    # starts game off with one D6
+                all_dice = [Dice(6, times_scaled)]    # starts game off with one D6
                 # initializes button to roll dice
             if roll_button.rect.collidepoint(event.pos) and dice_option.clicked:
                 for dice in all_dice:
                     dice.roll_dice()
+                dice_total = add_dice_total(all_dice)
+                display_dice_total = display_font.render(f"{dice_total}", True, (255, 255, 255))
             if add_dice_button.rect.collidepoint(event.pos) and dice_option.clicked:
-                all_dice.append(Dice(6))
+                all_dice.append(Dice(6, times_scaled))
                 new_die_added = True
             # elif slot_option.rect.collidepoint(event.pos):
             #     print("SLOTS")
 
     if new_die_added:
-        die_position_correct(all_dice, size)
+        if die_position_correct(all_dice, size) == "rerun":
+            die_position_correct(all_dice, size)
+            times_scaled += 1
+            # preforms a rerun of correcting the positions of the die as they have then been scaled down
         new_die_added = False
     # do not blit above #
     screen.fill((0, 0, 0))
@@ -85,6 +102,7 @@ while run:
         screen_blit_die(all_dice)
         screen.blit(roll_button.image, roll_button.rect)
         screen.blit(add_dice_button.image, add_dice_button.rect)
+        screen.blit(display_dice_total, (0, 0))
     else:
         screen.blit(dice_option.image, dice_option.rect)
 
