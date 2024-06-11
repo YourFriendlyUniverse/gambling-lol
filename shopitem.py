3b7734fb3767c46657fa0508d425cd560602709a
YourFriendlyUniverse <145475832+YourFriendlyUniverse@users.noreply.github.com>
2024-06-11 11:01:25 -0400

    Add files via upload

--- shopitem.py
+++ shopitem.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
 
 class ShopItem:
-    def __init__(self, name):
+    def __init__(self, name, multiplier):
         self.name = name
         self.clicked = False
         self.image = pygame.image.load(f"ShopItems/{self.name}_ShopItem.png")
@@ -10,10 +11,9 @@ class ShopItem:
         self.rect = pygame.Rect(0, 0, self.image_size[0], self.image_size[1])
         self.rect.center = (100, 100)
         self.center = self.rect.center
-        self.is_bought = False
+        self.multiplier = multiplier
+        self.price = 1 * multiplier
 
-    def buy(self):
-        self.is_bought = True
-
-    def change_pos(self, x, y):
-        self.rect.center = (x, y)
+    def set_price(self, original_price):
+        self.multiplier = int(self.multiplier)
+        self.price = (original_price * self.multiplier) + round(random.randint(1, 10) * self.multiplier / random.randint(1, round((self.multiplier / 2) + 1)))
